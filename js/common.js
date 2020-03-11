68d2e5593acc0252b38f045052d86f72ac90d64d
bobo <875792791@qq.com>
2020-03-11 21:46:52 +0800

    home0311

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 //公有弹出层方法
+
     function alertForm(title,id,callback){
         layer.open({
             title:title,
@@ -59,3 +60,102 @@
                 
               })
                 }
+
+                function showPerson(){
+                  let s = document.getElementById("showLogout")
+                  s.style.visibility = ""
+                }
+                function hiddenPerson(){
+                  let s = document.getElementById("showLogout")
+                  s.style.visibility = "hidden"
+                }
+                function logout(){
+                  // realLogout();
+                  
+                  quesForm('你確定要退出登錄嗎?',realLogout)
+                }
+                function realLogout(){
+                  sessionStorage.removeItem("status")
+                  layer.closeAll()
+                  ifLogin();
+                }
+                //用户是否登录  status：0.未登录  1.已登陆
+                function ifLogin(){
+                  //TODO  sessionStorage.setItem("status", 0);放在login方法里
+                  // sessionStorage.setItem("status", 0);
+                  var value = sessionStorage.getItem("status");
+                  console.log(value)
+                  if(!value){
+                    document.getElementById("notLogin").style.display="";//显
+                    document.getElementById("logged").style.display="none";//隱
+                    window.history.go(-1)
+                    layer.msg('你已退出登錄', {icon: 2})
+                    
+                  }else{
+                    document.getElementById("notLogin").style.display="none";//隐
+                    document.getElementById("logged").style.display="";//显
+                    layer.msg('登錄成功', {icon: 1})
+                  }   
+                }
+                //登錄表單驗證
+                function validateLogin(form){
+                  var form = layui.form;
+                  form.verify({
+                        lusername:function(value){
+                          if(value.length==0){
+                             return '用戶名不能爲空'
+                           }
+                        },
+                        lpassword: function(value) {
+                           if(value.length==0){
+                             return '密碼不能爲空'
+                        }
+                      }
+                    });
+                }
+               
+                //註冊表單驗證
+                function validateRegister(form){
+                  form.verify({
+                    rusername:function(value){
+                      if(value.length==0){
+                         return '用戶名不能爲空'
+                        }
+                    },
+                    rpassword:function(value){
+                      if(value.length<6){
+                         return '密码不能小於6位'
+                        }
+                    },
+                    // rpassword:  [
+                    //   /^[\S]{6,12}$/,
+                    //   '密码必须6到12位，且不能出现空格'
+                    // ],
+                    });
+                }
+    
+                function clickLogin(){
+                  alertcomfirm("loginLayer","登录","#loginId",'loginVerify',validateLogin,ajaxLogin)
+                }
+                function clickRegister(){
+                  alertcomfirm("registerLayer","註冊","#registerId",'registerVerify',validateRegister,ajaxRegister)
+                }
+                function ajaxLogin(data){
+                  console.log(data)
+                  //如果成功，將status寫入session
+                  sessionStorage.setItem("status", 1);
+                  layer.closeAll()
+                  ifLogin()
+                }
+                function ajaxRegister(data){
+                  console.log("ajaxRegister",data)
+                  if(data.rpassword!==data.rcheckPassword){
+                    layer.msg('兩次輸入的密碼不一致', {icon: 5});
+                    return false;
+                  }
+                  layer.closeAll()
+                }
+                function clickTitle(){
+                  alert("即将前往首页")
+                }
+                
