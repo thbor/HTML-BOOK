349da5896988ace65e36190ceda31eb4f7aaf61a
bobo <875792791@qq.com>
2020-03-12 11:28:25 +0800

    fix:登錄退出時iframe與layer的z-index衝突問題

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-//公有弹出层方法
+
     function alertForm(title,id,callback){
         layer.open({
             title:title,
@@ -12,50 +12,69 @@
     }
     function quesForm(content,callback){
       layer.confirm(content, {
-        yes: function(index, layero){
-          callback()
+        btn: ['確定','取消'] //按钮
+        ,cancel: function(index, layero){
+        returnParentListen();
+        console.log("cancel")
         }
+      }, function(){
+        //yes
+        callback()
+      }, function(){
+        returnParentListen();
+        console.log("no")
       });
-    }
+      }
 
-                //彈出窗口模板
-                function alertcomfirm(id,title,contentId,layFilter,validateMethod,ajaxMethod){
-                  layui.use(['form','layer'], function(){
-                    var form = layui.form;
-                    //數據驗證  
-                    validateMethod(form)
-                    layer.open({
-                    //type：1  這樣才不會在數據驗證失敗後關閉彈出層
-                    type:1,   
-                    area: '350px',
-                    title:title,
-                    id:id,
-                    content: $(contentId).html()
-                    ,btn: ['确定', '取消']
-                    //點擊登錄
-                    ,success: function(layero, index){
-                      layero.addClass('layui-form');
-                      layero.find('.layui-layer-btn0').attr({
-                                  'lay-filter': layFilter,
-                                  'lay-submit': ''
-                              });
-                      //重新渲染form
-                      form.render();
-                      layer.iframeAuto(index);
-                    },
-                    //點擊確定
-                    yes: function(index, layero){
-                      form.on('submit('+layFilter+')', function (data) {
-                        // layer.alert(JSON.stringify(data.field))
-                        //TODO   ajax 
-                        ajaxMethod(data.field)
-                        console.log("yes")
-                        // console.log(data)
-                        return false
-                        // layer.closeAll()
-                      })
-                    },
-                })
-                
-              })
-                }
+    //彈出窗口模板
+    function alertcomfirm(id,title,contentId,layFilter,validateMethod,ajaxMethod){
+      layui.use(['form','layer'], function(){
+        var form = layui.form;
+        //數據驗證  
+        validateMethod(form)
+        layer.open({
+        //type：1  這樣才不會在數據驗證失敗後關閉彈出層
+        type:1,   
+        area: '350px',
+        title:title,
+        id:id,
+        content: $(contentId).html()
+        ,btn: ['确定']
+        //點擊右上角X
+        ,cancel:function(){
+          returnParentListen();
+        }
+        //點擊登錄
+        ,success: function(layero, index){
+          layero.addClass('layui-form');
+          layero.find('.layui-layer-btn0').attr({
+          'lay-filter': layFilter,
+          'lay-submit': ''
+          });
+        //重新渲染form
+        form.render();
+          parentListen();
+        },
+        //點擊確定
+        yes: function(index, layero){
+        form.on('submit('+layFilter+')', function (data) {
+        // layer.alert(JSON.stringify(data.field))
+        //TODO   ajax 
+        ajaxMethod(data.field)
+        console.log("yes")
+        returnParentListen();
+        // console.log(data)
+        return false
+        // layer.closeAll()
+        })
+        
+        },
+      })
+    })
+  }
+    function parentListen(){
+      parent.changeZindex()
+    }
+    function returnParentListen(){
+      parent.returnZindex()
+    }
